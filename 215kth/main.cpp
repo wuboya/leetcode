390d8d0c1c8bb70a38936d10aa1fca83bb06c7b3
Mo Tao <mythly@qq.com>
2016-03-28 20:11:49 +0800

    another solution for 215kth, an good example for STL and lambda expression

--- 215kth/main.cpp
+++ 215kth/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
@@ -18,7 +19,8 @@ public:
         while (p < r) {
             int len = r - p + 1;
             swap(nums[p + rand() % len], nums[r]);
-            int pos = partition(nums, p, r);
+            //int pos = partition(nums, p, r);
+			int pos = partition2(nums, p, r);			
             if (pos == k - 1) return nums[pos];
             if (pos < k - 1) {
                 p = pos + 1;
@@ -44,6 +46,13 @@ private:
 
         return i + 1;
     }
+
+	int partition2(vector<int>& nums, int p, int r) {
+		int x = nums[r];
+		auto it = std::partition(nums.begin() + p, nums.begin() + r, [x](int t) { return t >= x; });
+		swap(nums[r], *it);
+		return it - nums.begin();
+	}
 };
 
 int main(int argc, const char * argv[]) {
@@ -55,6 +64,13 @@ int main(int argc, const char * argv[]) {
     //v.push_back(4);
     //v.push_back(5);
     //v.push_back(6);
-    cout << s.findKthLargest(v, 2);
+    cout << s.findKthLargest(v, 2) << endl;
+
+	v.clear();
+	for (int i = 1; i <= 10; ++i)
+		v.push_back(i);
+	random_shuffle(v.begin(), v.end());
+	cout << s.findKthLargest(v, 5) << endl;
+
     return 0;
 }
