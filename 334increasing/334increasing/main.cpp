0c85d3ea19ff0399ade3898fa7eb02a74197aa1a
wuboya <stella.1991@163.com>
2016-04-28 15:08:56 +0800

    check whether there exists increasing triplet subsequence (better version)

--- 334increasing/334increasing/main.cpp
+++ 334increasing/334increasing/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {// 类似最长递增子序列
-        int len = nums.size();
+        /*int len = nums.size();
         vector<int> f(len + 1, 1);// f(i)为包含i的最长递增子序列，f(i) = max(f(j) + 1), j < i && nums[j] < nums[i]
         for (int i = 1; i < len; ++ i) {
             for (int j = i - 1; j >= 0; -- j) {
@@ -25,6 +25,15 @@ public:
                     return true;
                 }
             }
+        }
+        return false;*/
+        int small = INT_MAX - 1, big = INT_MAX;
+        for (int i = 0; i < nums.size(); ++ i) {
+            if (nums[i] <= small) {// nums[i] <= small < big
+                small = nums[i];
+            } else if (nums[i] <= big) {// small < nums[i] <= big
+                big = nums[i];
+            } else return true;// small < big < nums[i]
         }
         return false;
     }
